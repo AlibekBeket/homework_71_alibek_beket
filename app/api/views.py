76bbf1ceb20230bdf735d95cbdc3b7607b68a32c
Alibek Beket <alibek1beket@gmail.com>
2023-04-21 17:06:58 +0600

    добавил во api/views.py класс PostsView которая наследуется от ModelViewSet

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from instagram.models import Posts
+
+from api.serializers import PostsSerializer
+
 
 # Create your views here.
+
+
+class PostsView(ModelViewSet):
+    queryset = Posts.objects.all()
+    serializer_class = PostsSerializer
